fb0a5d71e28d76f4df77bf49f64066cf404c336f
kitaharakasusa <894655469@qq.com>
2019-09-09 13:15:04 +0800

    add some

--- ehtf-idf.py
+++ ehtf-idf.py
@@ -1,5 +1,8 @@
 import jieba
 import math
+import re
+import numpy as np
+regex=re.compile('[%s]' % re.escape('[\s+\.\!\/_,$%^*(+\"\'“”’‘]+|[+——！，-。╮╯◇？、· 【〜～~@#￥%……&*（）]+?✌😉'))
 
 
 def getEFTF(document):
@@ -12,7 +15,7 @@ def getEFTF(document):
         TNAAW += count[key]
     for key in count.keys():
         count[key] = count[key] / TNAAW
-    print(count)
+
     return count
 
 
@@ -21,21 +24,24 @@ def getEFIDF(document):
     weibo_list = document
     newlist = []
     for line in weibo_list:
-        newlist.append(line)
+        newline = []
+        for word in line:
+            newline.append(word)
+        newlist.append(newline)
 
     for i, sentence in enumerate(weibo_list):
         for j, key in enumerate(sentence):
             NPD = 0
-            for j in range(0, i):
-                for word in weibo_list[j]:
+            for k in range(0, i):
+                for word in weibo_list[k]:
                     if word == key:
                         NPD += 1
             TNPD = i
             if NPD == 0:
                 NPD = 1
-            print(key)
+            # print(key)
             newlist[i][j] = {key: math.log((TNPD+1) / NPD) + 1}
-    return weibo_list
+    return newlist
 
 
 def get_current_EFTFIDF():
@@ -43,14 +49,38 @@ def get_current_EFTFIDF():
     document = ''
     weibo_list = []
     for line in file:
-        after_cut = ' '.join(jieba.cut(line.replace('\n', '')))
-        document += after_cut
+        content = regex.sub("", line.replace('\n', ''))
+        after_cut = ' '.join(jieba.cut(content))
+        print(after_cut)
+        document += after_cut + ' '
         line_list = after_cut.split(' ')
+        print(line_list)
         weibo_list.append(line_list)
+    print(weibo_list)
     TF = getEFTF(document)
     IDF = getEFIDF(weibo_list)
-    print(TF)
-    print(IDF)
+    print('TF', TF)
+    print('IDF', IDF)
+
+    for i, line in enumerate(IDF):
+        for j, word in enumerate(line):
+            pass
+            # print(word.keys())
+            index = list(word.keys())[0]
+            IDF[i][j] = {index: word[index] * TF[index]}
+
+    TF_IDF = IDF
+
+def getTEAandEV(TF_IDF):
+    TEA = set()
+    all_list = []
+    for line in TF_IDF:
+        for word_dict in line:
+            for key in word_dict.keys():
+                all_list.append(word_dict[key])
+
+    TEA.add(np.mean(all_list))
+
 
 
 if __name__ == '__main__':
